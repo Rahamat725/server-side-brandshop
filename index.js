12a8d3e64dc6c3a03cbf095f14fa87c7327f6b8f
101379670 <skrahamatullah500@gmail.com>
2023-11-16 18:49:11 +0530

    POST Operation Created

--- index.js
+++ index.js
@@ -30,6 +30,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+     const ProductCollection = client.db('BrandDB').collection("brands");
+
+     app.post('/brands', async(req,res) => {
+      const addProducts = req.body;
+      const result = await ProductCollection.insertOne(addProducts);
+      res.send(result);
+     })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
